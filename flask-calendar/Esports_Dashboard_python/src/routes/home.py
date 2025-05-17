9f0da78318b9a400a3998e2a281c306a39cae131
DVanSteenwyk <vansteenwykdrake@gmail.com>
2025-05-17 17:16:10 -0600

    Delete flask-calendar directory

--- flask-calendar/Esports_Dashboard_python/src/routes/home.py
+++
@@ -1,9 +0,0 @@
-# src/routes/home.py
-
-from flask import Blueprint, render_template
-
-home_bp = Blueprint('home', __name__)
-
-@home_bp.route('/')
-def home():
-    return render_template('home.html')
